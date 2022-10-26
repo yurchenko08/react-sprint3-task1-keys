073a4c53d308fda6afb781a1fd8c0e7d297f273f
yurchenko08 <yurchenko08@gmail.com>
2022-10-26 18:11:19 +0300

    finished 1 task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
+  const list = [
+    { id: 1, text: "Animals" },
+    { id: 2, text: "Anime" },
+    { id: 3, text: "Anti-Malware" },
+    { id: 4, text: "Art Design" },
+    { id: 5, text: "Books" },
+    { id: 6, text: "Business" },
+    { id: 7, text: "Calendar" },
+    { id: 8, text: "Cloud Storage" },
+    { id: 9, text: "File Sharing" },
+    { id: 10, text: "Animals" },
+    { id: 11, text: "Continuous Integration" },
+    { id: 12, text: "Cryptocurrency" },
+  ];
   return (
     <div>
       Some List:
+      <ul>
+        {list.map((el) => (
+          <li key={el.id}>{el.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
